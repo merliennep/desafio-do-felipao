46136cff9e0fd00ae9e60cc795b2b98e72abe083
Merlienne <merliennep@hotmail.com>
2024-05-15 23:50:20 -0300

    Desafio Surpresa do Felipao

---
+++ desafio-extra-felipao.js
@@ -0,0 +1,18 @@
+/*Crie um código para armazenar o máximo de informações possível dos pokemon
+da imagem ao lado e ao final, para cada um, exiba uma mensagem de saída escrita
+o nome do pokemon concatenado com "Cadastrado com sucesso"*/
+
+
+let meusPokemons = [
+    ["Poochyena", "masculino", 2, 13],
+    ["Zigzagoon", "feminino", 2, 13],
+    ["Dragonite", "masculino", 2, 15],
+    ["Dragonite", "feminino", 2, 15],
+    ["Dragonite", "feminino", 2, 15],
+    ["Poochyena", "feminino", 2, 15],
+    ["Wurmple", "masculino", 2, 15]
+]
+
+console.log("Os Pokemons " + meusPokemons[0][0] + ", " + meusPokemons[1][0] +  ", " + meusPokemons[2][0] + ", " 
++ meusPokemons[3][0] + ", " + meusPokemons[4][0] + ", " + meusPokemons[5][0] + " e " + meusPokemons[6][0] + " " 
++ " foram cadastrados com sucesso!")
